a5de46d8d6f79e1276875d07732872e776ac0a50
soykansaydam07 <soykansaydam1@gmail.com>
2024-02-08 00:03:11 +0300

    .

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -20,7 +20,7 @@ namespace Routing_Mechanism
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container..
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -46,11 +46,44 @@ namespace Routing_Mechanism
 
             app.UseAuthorization();
 
+
+            //MapControllerRoute: Custom olarak bizim istediğimiz şekilde oluşturulmasını sağladığımız route yapılanmasıdır 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                //Default olarak verilen değer MapDefaultControllerRoute tarafı ile aynı şekilde olduğu gözükmekte
+                //endpoints.MapControllerRoute(
+                //    name: "default",
+                //    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                //Rota şablon varsa {} parametrelere karşılık gelmektedir , controller ve action parametreleri MapDefaultControllerRoute içinde tanmlanmıştır diğerleri ise custom anımlanmış anlamındadır
+                //MapDefaultControllerRoute için hiç bir parametre olmadan bir tetikleme yapmak istiyorsam  controller home olarak action da index olarak tetiklenecektir 
+
+                //MapDefaultControllerRoute: Default route oluşturmak için kullanılan yapıdır 
+                //endpoints.MapDefaultControllerRoute();
+
+                //Her bir controller route, default da bir ismi olması gerekmektedir name parametresi bundan kullanılır, Diğer pattern route bazında nasıl yönlenme yapılcağına dair verilen parametrelerdir 
+                //endpoints.MapControllerRoute("Default", "{controller=Personel}/{action=Index}");
+
+                //Bunun yanında parametre olarak default değer anasayfa olarak gelmesini istersek yapılması gereken ,3 parametre bir instance şeklinde  default değeri doldurulacaktır böylece farklı bir isimde default parametresi verilebilecektir  
+                //Anasayfa parametresi verildiğinde default parametre çalıştırılıcaktır 
+                //endpoints.MapControllerRoute("DefaultSecond", "anasayfa",new {controller = "Home", action = "Index"});
+
+                //Birden fazla route yapılandırması oluşturucaksınız bu yapılanma özelden geneel doğru aşağıya yazılarak gitmesi sağlanmaktadir çünkü özel olan bu yapılandırmada kullanılamıyorsa genel olan çalıştırılarak sistemin hata vermesi önlenir 
+
+                //Bu kısımda Route Constraints dediğimiz bir yapı ile parametre olarak tanımlanan verilerin  sadece int gelmesini sağlamak gibi durumlar için kullanılan yapıdır 
+                //Mesala Aşağıdaki id parametresinini sadece id gelmesini sağlamak için bu yapı kullanılıcaktır , bildiğim kadarıyla string ifade yerine int kullanımı için genellikle yapılandırılır, diğer tarzlarda kullanımı mevcuttur
+                //Bunun yanında route parametrelerine değerler verilebilmektedir  x gelen verinin  12 karakterli olmasını istedik aşağıdaki örnekte mesala min max gibi yapılar kullanılabilir 
+                endpoints.MapControllerRoute("DefaultThird", "{controller=Home}/{action=Index}/{id:int?}/{x:alpha:length(12)}/{y?}");
+
+                //Bunun yanında CustomConstraint ile custom route yapılandırnalırı yapılabiliyor CustomContrait sınıfı oluşturulup IRouteConstrait interface ile Match metodu override edilir
+
+                endpoints.MapDefaultControllerRoute();
+
+                //Attribute Routing:  Geleneksel yaklaşım olarak startup cs tarafında yönetim sağladığımız merkezi hale getirdiğimiz durumlar , birde Attribute bazlı yaklaşım da controller bazlı hareket ile yapılandırma sağlama olayı
+                endpoints.MapControllers(); //Route için gelen isteği attribute taraflı  controller verileri ile eşleştir , bunu yapmadan controller daki rouelarda çalışma olmaz 
+                                            //Controller bazlı tanımlamalar ["[controller]/[action]"] şeklinde tanımlı ifadeler köşeli tanımsızlar için süslü parantez, route için şablonlarda değişken için doğrudan süslü parantez kullanımı bulunmaktadır 
+
+                //Api yapılandırmalırında  Controlelr bazlı Route kullanlırken mvc tarafında konvensiyonel bir yaklaşım sergilenmektedir 
             });
         }
     }
